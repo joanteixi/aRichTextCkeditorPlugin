c3b6749d909bdc8635257741413464904ed004f0
joanteixido <joan@laiogurtera.com>
2011-02-12 00:22:34 +0100

    Added options to select the toolbar. Also improved code.

--- lib/form/aRichTextCkeditorSlotEditForm.class.php
+++ lib/form/aRichTextCkeditorSlotEditForm.class.php
@@ -28,15 +28,21 @@ class aRichTextCkeditorSlotEditForm extends BaseForm
   public function configure()
   {
     $widgetOptions = array();
-    $tool = $this->consumeSlotOption('tool');
-    if (!is_null($tool))
+    $toolbar = $this->consumeSlotOption('toolbar');
+    $jsoptions = $this->consumeSlotOption('jsoptions');
+    if (!is_null($toolbar))
     {
-      $widgetOptions['tool'] = $tool;
+      $widgetOptions['toolbar'] = $toolbar;
+    }
+    if (!is_null($jsoptions))
+    {
+      $widgetOptions['jsoptions'] = $jsoptions;
     }
     // The rest of the options passed become attributes of the widget
 //    $this->setWidgets(array('value' => new aWidgetFormRichTextarea($widgetOptions, $this->soptions)));
     $this->setWidgets(array('value' => new sfWidgetFormCKEditor($widgetOptions, $this->soptions)));
     $this->setValidators(array('value' => new sfValidatorHtml(array('required' => false, 'allowed_tags' => $this->allowedTags, 'allowed_attributes' => $this->allowedAttributes, 'allowed_styles' => $this->allowedStyles))));
+
     // There are problems with AJAX plus FCK plus Symfony forms. FCK insists on making the name and ID
     // the same and brackets are not valid in IDs which can lead to problems in strict settings
     // like AJAX in IE. Work around this by not attempting to use brackets here
--- lib/widget/sfWidgetFormCKEditor.class.php
+++ lib/widget/sfWidgetFormCKEditor.class.php
@@ -6,66 +6,65 @@ class sfWidgetFormCKEditor extends sfWidgetFormTextarea {
   protected $_finder;
 
   protected function configure($options = array(), $attributes = array()) {
+
+
     $this->addOption('editor', 'ck');
     $this->addOption('css', false);
-    $this->addOption('tool', 'basic');
+    $this->addOption('toolbar', 'basic');
     $this->addOption('height', '225');
     $this->addOption('width', '100%');
-    $path = sfConfig::get('sf_rich_text_ck_js_dir','aRichTextCkeditorPlugin/js/ckeditor');
-    $php_file =  $path . DIRECTORY_SEPARATOR . 'ckeditor.php';
+    $this->addOption('jsoptions', null);
+
+    parent::configure($options, $attributes);
+  }
+
+  public function render($name, $value = null, $attributes = array(), $errors = array()) {
+    $path = sfConfig::get('sf_rich_text_ck_js_dir', 'aRichTextCkeditorPlugin/js/ckeditor');
+    $php_file = $path . DIRECTORY_SEPARATOR . 'ckeditor.php';
 
     if (!is_readable(sfConfig::get('sf_web_dir') . DIRECTORY_SEPARATOR . $php_file)) {
       throw new sfConfigurationException('You must install CKEditor to use this widget (see rich_text_ck_js_dir settings). ' . sfConfig::get('sf_web_dir') . DIRECTORY_SEPARATOR . $php_file);
     }
     // CKEditor.php class is written with backward compatibility of PHP4.
     // This reportings are to turn off errors with public properties and already declared constructor
-//    $error_reporting = error_reporting(E_ALL);
-
+    $error_reporting = error_reporting(E_ALL);
     require_once(sfConfig::get('sf_web_dir') . DIRECTORY_SEPARATOR . $php_file);
-
     // turn error reporting back to your settings
-//    error_reporting($error_reporting);
+    error_reporting($error_reporting);
+
     $editorClass = 'CKEditor';
     if (!class_exists($editorClass)) {
       throw new sfConfigurationException(sprintf('CKEditor class not found'));
     }
+
     $this->_editor = new $editorClass();
-    $this->_editor->basePath = sfContext::getInstance()->getRequest()->getRelativeUrlRoot().DIRECTORY_SEPARATOR.sfConfig::get('app_ckeditor_basePath',$path.'/');
+    $this->_editor->basePath = sfContext::getInstance()->getRequest()->getRelativeUrlRoot() . DIRECTORY_SEPARATOR . sfConfig::get('app_ckeditor_basePath', $path . '/');
     $this->_editor->returnOutput = true;
-//    if (sfConfig::get('app_ckfinder_active', false) == 'true') {
-//      $finderClass = 'CKFinder';
-//      if (!class_exists($finderClass)) {
-//        throw new sfConfigurationException(sprintf('CKFinder class not found'));
-//      }
-//      $this->_finder = new $finderClass();
-//      $this->_finder->BasePath = sfConfig::get('app_ckfinder_basePath');
-//      $this->_finder->SetupCKEditorObject($this->_editor);
-//    }
-    if (isset($options['jsoptions'])) {
-      $this->addOption('jsoptions', $options['jsoptions']);
+
+    // Joan Teixidó: i didn't test if CKFinder can run here
+    if (sfConfig::get('app_ckfinder_active', false) == 'true') {
+      $finderClass = 'CKFinder';
+      if (!class_exists($finderClass)) {
+        throw new sfConfigurationException(sprintf('CKFinder class not found'));
+      }
+      $this->_finder = new $finderClass();
+      $this->_finder->BasePath = sfConfig::get('app_ckfinder_basePath');
+      $this->_finder->SetupCKEditorObject($this->_editor);
     }
+
+    // witch toolbar render.
 
     $this->configure_options = array();
-    $this->configure_options['toolbar'] = $this->getToolbar($this->getOption('tool'));
-    parent::configure($options, $attributes);
-  }
+    $this->configure_options['toolbar'] = $this->getToolbar($this->getOption('toolbar'));
 
-  public function render($name, $value = null, $attributes = array(), $errors = array()) {
-    $jsoptions = $this->getOption('jsoptions');
-    if ($jsoptions) {
-      $this->setJSOptions($name, $value, $attributes, $errors);
+    if ($jsoptions = $this->getOption('jsoptions')) {
+      foreach ($jsoptions as $k => $v) {
+        $this->_editor->config[$k] = $v;
+      }
     }
-
-//    $content = $this->_editor->editor('ckeditor');
     return parent::render($name, $value, $attributes, $errors) . $this->_editor->replace($name, $this->configure_options);
   }
 
-  protected function setJSOptions($name, $value = null, $attributes = array(), $errors = array()) {
-    $jsoptions = $this->getOption('jsoptions');
-    foreach ($jsoptions as $k => $v) {
-      $this->_editor->config[$k] = $v;
-    }
-  }
 
   public function getEditor() {
     return $this->_editor;
@@ -75,25 +74,39 @@ class sfWidgetFormCKEditor extends sfWidgetFormTextarea {
     return $this->_finder;
   }
 
-  protected function getToolbar($type)
-  {
-    switch($type)
-    {
-       case 'full':
-        return array();
-        break;
-
-      
-      default:
+  /**
+   * This function returns an array of options that define the toolbar.
+   * Maybe it'll be better to load js file (see documentation of ckEditor)
+   *
+   * @param String $type
+   * @return array
+   */
+  protected function getToolbar($type) {
+    switch ($type) {
+      case 'full':
         $r = array(
-	      array( 'Source', '-', 'Bold', 'Italic', 'Underline', 'Strike' ),
-	      array( 'Link', 'Unlink', 'Anchor' )
-            );
+            array('Source', '-', 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord'),
+            array('Undo', 'Redo', 'SelectAll', 'RemoveFormat', '-', 'Replace'),
+            array('Bold', 'Italic', 'Underline', 'Strike', '-'),
+            array('NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', 'Blockquote', 'CreateDiv'),
+            array('JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'),
+            array('Link', 'Unlink', 'Anchor'),
+            array('Image', 'Flash', 'MediaEmbed', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak'),
+            array('Styles', 'Format', 'Font', 'FontSize'),
+            array('TextColor', 'BGColor'),
+            array('Maximize', 'ShowBlocks', '-', 'About')
+        );
         return $r;
         break;
 
 
-
+      default:
+        $r = array(
+            array('Source', '-', 'Bold', 'Italic', 'Underline', 'Strike'),
+            array('Link', 'Unlink', 'Anchor')
+        );
+        return $r;
+        break;
     }
   }
 
